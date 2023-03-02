b78cba921c2fe50d1797916f9e047dfcb2eada31
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-03-02 14:02:45 +0530

    find winner of game

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  // state to check whose turn it is
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if(squares[i])
+      return;
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner();
 
   return (
     <>
+      <div className="winner-row">
+        <h3>Winner : <span className='winner-player'>{winner}</span></h3>
+      </div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -36,4 +46,34 @@ export default function Board() {
       </div>
     </>
   );
+
+  // decide winner
+  function calculateWinner() {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for(let line of lines) {
+      const [a,b,c] = line;
+      if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c])
+        return squares[a];
+      else {
+        let isEmptyCount = 0;
+        for(let i of squares) {
+          if(!squares[i]) {
+            isEmptyCount++;
+          }
+        }
+        if(isEmptyCount === 0) {
+          return "Draw !";
+        }
+      }
+    }
+  }
 }
