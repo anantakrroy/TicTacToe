ee35949136522ae108da0ba0a3f04dacf4209896
Ananta Kr. Roy <kr.innovate@gmail.com>
2023-03-07 13:13:58 +0530

    implement game history

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
   // decide whose move it is
   function handleClick(i) {
-    if (squares[i]) return;
+    if (calculateWinner(squares) || squares[i]) return;
     const nextSquares = squares.slice();
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     onPlay(nextSquares);
@@ -81,12 +81,34 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0); // X's move is even numbered since it starts first
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move : ${move}`;
+    } else {
+      description = `Go to game start`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -94,7 +116,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-history">
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
